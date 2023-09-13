dec998e6239fa80a955318f7f6cab9dbeb997047
Nelson Estevão <nelsonmestevao@proton.me>
2023-09-13 12:08:40 +0100

    chore: format html in example code

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,7 +1,16 @@
 defmodule Example do
   use Francis
 
-  get("/", fn _ -> "<html><body><h1>Hello, world!</h1></body></html>" end)
+  get("/", fn _ ->
+    """
+    <html>
+     <body>
+       <h1>Hello, world!</h1>
+     </body>
+    </html>
+    """
+  end)
+
   get("/:name", fn %{params: %{"name" => name}} -> "hello #{name}" end)
 
   ws("ws", fn "ping" -> "pong" end)
