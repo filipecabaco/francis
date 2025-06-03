22932da044f6c26edebcdaf151e3df2f450da1e9
Filipe Cabaço <filipecabaco@gmail.com>
2025-06-03 23:33:38 +0100

    move template to module

--- lib/mix/tasks/francis/new.ex
+++ lib/mix/tasks/francis/new.ex
@@ -3,6 +3,84 @@ defmodule Mix.Tasks.Francis.New do
 
   @shortdoc "Generates a new Francis project"
 
+  @mix_template """
+  defmodule <%= Macro.camelize(module_name) %>.MixProject do
+    use Mix.Project
+
+    def project do
+      [
+        app: :<%= app_name %>,
+        version: "0.1.0",
+        elixir: "~> 1.18",
+        start_permanent: Mix.env() == :prod,
+        deps: deps(),
+        elixirc_paths: ["lib"]
+      ]
+    end
+
+    def application do
+      [mod: {<%= Macro.camelize(module_name) %>, []}, extra_applications: [:logger]]
+    end
+
+    defp deps do
+      [
+        {:francis, "~> 0.1"}
+      ]
+    end
+  end
+  """
+
+  @gitignore_template """
+  # The directory Mix will write compiled artifacts to.
+  /_build/
+
+  # If you run "mix test --cover", coverage assets end up here.
+  /cover/
+
+  # The directory Mix downloads your dependencies sources to.
+  /deps/
+
+  # Where 3rd-party dependencies like ExDoc output generated docs.
+  /doc/
+
+  # Ignore .fetch files in case you like to edit your project deps locally.
+  /.fetch
+
+  # If the VM crashes, it generates a dump, let's ignore it too.
+  erl_crash.dump
+  """
+
+  @with_sup_app_template """
+  defmodule <%= Macro.camelize(module_name) %> do
+    use Application
+
+    def start(_type, _args) do
+      children = [
+        <%= Macro.camelize(module_name) %>.Router
+      ]
+
+      opts = [strategy: :one_for_one, name: <%= Macro.camelize(module_name) %>.Supervisor]
+      Supervisor.start_link(children, opts)
+    end
+  end
+  """
+
+  @with_sup_router_template """
+  defmodule <%= Macro.camelize(module_name) %>.Router do
+    use Francis
+    get("/", fn _ -> "ok" end)
+    unmatched(fn _ -> "not found" end)
+  end
+  """
+
+  @without_sup_app_template """
+  defmodule <%= Macro.camelize(module_name)%> do
+    use Francis
+    get("/", fn _ -> "ok" end)
+    unmatched(fn _ -> "not found" end)
+  end
+  """
+
   @moduledoc """
   Generates a new Francis project.
   This task creates a new directory with the specified application name and a basic project structure.
@@ -43,7 +121,6 @@ defmodule Mix.Tasks.Francis.New do
       OptionParser.parse!(opts, strict: [sup: :boolean, supervisor_module_name: :string])
 
     File.mkdir_p!(app_name)
-    root = Path.expand("../../tasks/francis", __DIR__)
 
     module_name =
       if supervisor_module_name == [],
@@ -51,39 +128,39 @@ defmodule Mix.Tasks.Francis.New do
         else: hd(supervisor_module_name)
 
     # Copy and render templates
-    copy_template("#{root}/new/mix.eex", "#{app_name}/mix.exs", %{
-      module_name: module_name,
-      app_name: app_name
-    })
+    copy_template(:mix, "#{app_name}/mix.exs", %{module_name: module_name, app_name: app_name})
+    copy_template(:gitignore, "#{app_name}/.gitignore", %{})
 
-    copy_template("#{root}/new/.gitignore", "#{app_name}/.gitignore", %{})
+    File.mkdir_p!("#{app_name}/lib")
 
     if sup != [] && hd(sup) do
-      File.mkdir_p!("#{app_name}/lib")
-
-      copy_template(
-        "#{root}/new/with_supervisor/application.eex",
-        "#{app_name}/lib/application.ex",
-        %{module_name: module_name}
-      )
-
-      copy_template("#{root}/new/with_supervisor/router.eex", "#{app_name}/lib/router.ex", %{
+      copy_template(:with_sup_app, "#{app_name}/lib/application.ex", %{module_name: module_name})
+      copy_template(:with_sup_router, "#{app_name}/lib/router.ex", %{module_name: module_name})
+    else
+      copy_template(:without_sup_app, "#{app_name}/lib/#{app_name}.ex", %{
         module_name: module_name
       })
-    else
-      File.mkdir_p!("#{app_name}/lib")
-
-      copy_template(
-        "#{root}/new/without_supervisor/application.eex",
-        "#{app_name}/lib/#{app_name}.ex",
-        %{module_name: module_name}
-      )
     end
   end
 
-  defp copy_template(src, dest, assigns) do
-    content = File.read!(src)
-    rendered = EEx.eval_string(content, assigns)
+  defp copy_template(:mix, dest, assigns), do: write_template(@mix_template, dest, assigns)
+
+  defp copy_template(:gitignore, dest, assigns),
+    do: write_template(@gitignore_template, dest, assigns)
+
+  defp copy_template(:with_sup_app, dest, assigns),
+    do: write_template(@with_sup_app_template, dest, assigns)
+
+  defp copy_template(:with_sup_router, dest, assigns),
+    do: write_template(@with_sup_router_template, dest, assigns)
+
+  defp copy_template(:without_sup_app, dest, assigns),
+    do: write_template(@without_sup_app_template, dest, assigns)
+
+  defp write_template(template, dest, assigns) do
+    # Make Macro available in the template context and merge assigns
+    bindings = [Macro: Macro] ++ Map.to_list(assigns)
+    rendered = EEx.eval_string(template, bindings)
     File.write!(dest, rendered)
   end
 end
---
+++ test/mix/tasks/francis/new_test.exs
@@ -0,0 +1,64 @@
+defmodule Mix.Tasks.Francis.NewTest do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureIO
+
+  @moduletag :tmp_dir
+
+  test "creates a new project with default options", %{tmp_dir: tmp_dir} do
+    File.cd!(tmp_dir, fn ->
+      app_name = "my_app"
+
+      assert capture_io(fn ->
+               Mix.Tasks.Francis.New.main([app_name])
+             end) =~ ""
+
+      assert File.dir?(app_name)
+      assert File.exists?(Path.join([app_name, "mix.exs"]))
+      assert File.exists?(Path.join([app_name, ".gitignore"]))
+      assert File.dir?(Path.join([app_name, "lib"]))
+      assert File.exists?(Path.join([app_name, "lib", "#{app_name}.ex"]))
+
+      # Check content
+      mix_content = File.read!(Path.join([app_name, "mix.exs"]))
+      assert mix_content =~ "defmodule MyApp.MixProject"
+      assert mix_content =~ ":my_app"
+    end)
+  end
+
+  test "creates a new project with supervisor option", %{tmp_dir: tmp_dir} do
+    File.cd!(tmp_dir, fn ->
+      app_name = "my_sup_app"
+
+      assert capture_io(fn ->
+               Mix.Tasks.Francis.New.main([app_name, "--sup"])
+             end) =~ ""
+
+      assert File.dir?(app_name)
+      assert File.exists?(Path.join([app_name, "lib", "application.ex"]))
+      assert File.exists?(Path.join([app_name, "lib", "router.ex"]))
+      app_content = File.read!(Path.join([app_name, "lib", "application.ex"]))
+      assert app_content =~ "use Application"
+      router_content = File.read!(Path.join([app_name, "lib", "router.ex"]))
+      assert router_content =~ "use Francis"
+    end)
+  end
+
+  test "raises if app already exists", %{tmp_dir: tmp_dir} do
+    File.cd!(tmp_dir, fn ->
+      app_name = "existing_app"
+      File.mkdir_p!(app_name)
+
+      assert_raise Mix.Error, ~r/already exists/, fn ->
+        Mix.Tasks.Francis.New.main([app_name])
+      end
+    end)
+  end
+
+  test "raises if app name is invalid", %{tmp_dir: tmp_dir} do
+    File.cd!(tmp_dir, fn ->
+      assert_raise Mix.Error, ~r/must only contain alphanumeric/, fn ->
+        Mix.Tasks.Francis.New.main(["bad-app!"])
+      end
+    end)
+  end
+end
