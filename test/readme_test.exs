dffb1a25dacd7f985bb74ee197d9c9274852c43b
Nelson Estevão <nelsonmestevao@proton.me>
2025-06-11 11:34:48 +0100

    test: only check the minor

--- test/readme_test.exs
+++ test/readme_test.exs
@@ -3,8 +3,8 @@ defmodule ReadmeTest do
 
   @app Mix.Project.config()[:app]
 
-  test "version in README.md always matches the current #{@app} version" do
-    assert File.read!(Path.join(__DIR__, "../README.md")) =~
-             ~s'{:#{@app}, "~> #{Mix.Project.config()[:version]}"}'
+  test "version in README.md always matches the minor #{@app} version" do
+    app_version = Mix.Project.config()[:version] |> Version.parse!() |> Map.put(:patch, 0)
+    assert File.read!(Path.join(__DIR__, "../README.md")) =~ ~s'{:#{@app}, "~> #{Version.to_string(app_version)}"}'
   end
 end
